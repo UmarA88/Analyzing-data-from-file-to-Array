8d10728e83fc4561ff07b4325eb4734ff19990c3
Ahmed Matoussi <ahmedgeek1@gmail.com>
2017-12-21 13:53:49 -0600

    Add project files via upload
    Add all project files via upload first time adding the project

---
+++ astronomer records/Assessment9A/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Assessment9A
+{
+    partial class StarsBrightnessAnalysisApplicationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openStarsFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblNumberOfMatchesPrompt = new System.Windows.Forms.Label();
+            this.lblDimmerPrompt = new System.Windows.Forms.Label();
+            this.lblBrighterPrompt = new System.Windows.Forms.Label();
+            this.lblNumberOfMatches = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnOpenFile1 = new System.Windows.Forms.Button();
+            this.listBoxDimmerStars = new System.Windows.Forms.ListBox();
+            this.listBoxBrighterStars = new System.Windows.Forms.ListBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openStarsFileDialog
+            // 
+            this.openStarsFileDialog.FileName = "openStarsFileDialog";
+            // 
+            // lblNumberOfMatchesPrompt
+            // 
+            this.lblNumberOfMatchesPrompt.AutoSize = true;
+            this.lblNumberOfMatchesPrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberOfMatchesPrompt.Location = new System.Drawing.Point(346, 381);
+            this.lblNumberOfMatchesPrompt.Name = "lblNumberOfMatchesPrompt";
+            this.lblNumberOfMatchesPrompt.Size = new System.Drawing.Size(162, 19);
+            this.lblNumberOfMatchesPrompt.TabIndex = 0;
+            this.lblNumberOfMatchesPrompt.Text = "Number Of Matches";
+            // 
+            // lblDimmerPrompt
+            // 
+            this.lblDimmerPrompt.AutoSize = true;
+            this.lblDimmerPrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDimmerPrompt.Location = new System.Drawing.Point(357, 266);
+            this.lblDimmerPrompt.Name = "lblDimmerPrompt";
+            this.lblDimmerPrompt.Size = new System.Drawing.Size(124, 19);
+            this.lblDimmerPrompt.TabIndex = 1;
+            this.lblDimmerPrompt.Text = "Dimmer Values";
+            // 
+            // lblBrighterPrompt
+            // 
+            this.lblBrighterPrompt.AutoSize = true;
+            this.lblBrighterPrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBrighterPrompt.Location = new System.Drawing.Point(357, 112);
+            this.lblBrighterPrompt.Name = "lblBrighterPrompt";
+            this.lblBrighterPrompt.Size = new System.Drawing.Size(124, 19);
+            this.lblBrighterPrompt.TabIndex = 2;
+            this.lblBrighterPrompt.Text = "Brighter Values";
+            // 
+            // lblNumberOfMatches
+            // 
+            this.lblNumberOfMatches.BackColor = System.Drawing.Color.White;
+            this.lblNumberOfMatches.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNumberOfMatches.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberOfMatches.Location = new System.Drawing.Point(534, 370);
+            this.lblNumberOfMatches.Name = "lblNumberOfMatches";
+            this.lblNumberOfMatches.Size = new System.Drawing.Size(172, 30);
+            this.lblNumberOfMatches.TabIndex = 3;
+            this.lblNumberOfMatches.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Firebrick;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(148, 458);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(171, 49);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Firebrick;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(394, 458);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(166, 49);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnOpenFile1
+            // 
+            this.btnOpenFile1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnOpenFile1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile1.ForeColor = System.Drawing.Color.White;
+            this.btnOpenFile1.Location = new System.Drawing.Point(30, 250);
+            this.btnOpenFile1.Name = "btnOpenFile1";
+            this.btnOpenFile1.Size = new System.Drawing.Size(210, 49);
+            this.btnOpenFile1.TabIndex = 8;
+            this.btnOpenFile1.Text = "Open And Analyse a File";
+            this.btnOpenFile1.UseVisualStyleBackColor = false;
+            this.btnOpenFile1.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // listBoxDimmerStars
+            // 
+            this.listBoxDimmerStars.BackColor = System.Drawing.Color.White;
+            this.listBoxDimmerStars.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxDimmerStars.FormattingEnabled = true;
+            this.listBoxDimmerStars.ItemHeight = 17;
+            this.listBoxDimmerStars.Location = new System.Drawing.Point(534, 236);
+            this.listBoxDimmerStars.Name = "listBoxDimmerStars";
+            this.listBoxDimmerStars.Size = new System.Drawing.Size(172, 106);
+            this.listBoxDimmerStars.TabIndex = 9;
+            // 
+            // listBoxBrighterStars
+            // 
+            this.listBoxBrighterStars.BackColor = System.Drawing.Color.White;
+            this.listBoxBrighterStars.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxBrighterStars.FormattingEnabled = true;
+            this.listBoxBrighterStars.ItemHeight = 17;
+            this.listBoxBrighterStars.Location = new System.Drawing.Point(534, 93);
+            this.listBoxBrighterStars.Name = "listBoxBrighterStars";
+            this.listBoxBrighterStars.Size = new System.Drawing.Size(172, 106);
+            this.listBoxBrighterStars.TabIndex = 10;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(95, 21);
+            this.lblWelcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(521, 24);
+            this.lblWelcome.TabIndex = 22;
+            this.lblWelcome.Text = "Welcome To The Stars Brightness Analysis Application";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(597, 507);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "© Ahmed Matoussi";
+            // 
+            // StarsBrightnessAnalysisApplicationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(187)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(728, 541);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.listBoxBrighterStars);
+            this.Controls.Add(this.listBoxDimmerStars);
+            this.Controls.Add(this.btnOpenFile1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblNumberOfMatches);
+            this.Controls.Add(this.lblBrighterPrompt);
+            this.Controls.Add(this.lblDimmerPrompt);
+            this.Controls.Add(this.lblNumberOfMatchesPrompt);
+            this.Name = "StarsBrightnessAnalysisApplicationForm";
+            this.Text = "Stars Brightness Analysis Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openStarsFileDialog;
+        private System.Windows.Forms.Label lblNumberOfMatchesPrompt;
+        private System.Windows.Forms.Label lblDimmerPrompt;
+        private System.Windows.Forms.Label lblBrighterPrompt;
+        private System.Windows.Forms.Label lblNumberOfMatches;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnOpenFile1;
+        private System.Windows.Forms.ListBox listBoxDimmerStars;
+        private System.Windows.Forms.ListBox listBoxBrighterStars;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ astronomer records/Assessment9A/Form1.cs
@@ -0,0 +1,350 @@
+﻿/*Ahmed Matoussi
+ * email: ahmedgeek1@gmail.com
+ * This application is to read the data from a file and store it in a list of the appropriate size,
+ *  and then analyze its data
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Assessment9A
+{
+    public partial class StarsBrightnessAnalysisApplicationForm : Form
+    {
+        public StarsBrightnessAnalysisApplicationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            //clearing the label and the listboxes
+            lblNumberOfMatches.Text = "";
+            listBoxBrighterStars.Items.Clear();
+            listBoxDimmerStars.Items.Clear();
+            //declare an empty list
+            List<double> recordsList = new List<double>();
+            String nameOfStar = openStarsFile(recordsList); //declare a string variable to store the firstLine of the file which is the name of the star
+            String[,] knownValues = fileDataKnownValues(); //declare a 2D array and store the reference array given
+            Boolean error1 = false;
+            Boolean error2 = true; //booleans to verify the error type
+
+            foreach (string knownValue in knownValues)
+            {
+
+                if (knownValue != nameOfStar)
+                {
+                    error2 = true;
+                }
+                else
+                {
+                    error2 = false;
+                    break;
+                }
+            }
+            if (recordsList.Count == 0)
+            { //if there are no records in the file
+                MessageBox.Show("Sorry, there are no valid records to analyse!");
+            }
+            else if (error2)
+            {
+                MessageBox.Show("Star name does not exist in our known values records!");
+            }
+           /* else if (recordsList.Count > 1000)  //we can add this option if it were reauired, but in the assignment it doesn't say that it is required
+            {
+                MessageBox.Show("Your file contains more than 1000");
+            }*/
+            else
+            { //run a loop to verify if every record is valid or not
+                foreach (double record in recordsList)
+                {
+                    foreach (String knownValue in knownValues)
+                    {
+                        if (record < 0)
+                        { //if there is at least one negative record
+                            error1 = true;
+                            break;
+                        }
+                        if (nameOfStar != knownValue)
+                        {
+                            error2 = true;
+                        }
+
+                    }
+
+                    if (error1)
+                    { //throw error message
+                        MessageBox.Show("All the records in the file should be positive!");
+                        break;
+                    }
+                }
+            }
+            double exactValueMatching = findTheMatchingValueWithTheExactStar(nameOfStar, knownValues); //find the reference value from the reference manually entered table based on the star name
+            String numOfMatchesString = numberOfMatches(exactValueMatching, recordsList).ToString(); //declare a variable and store the number of matches in it   
+            if (numOfMatchesString == "0")
+            {
+                lblNumberOfMatches.Text = "No Matches";
+            }
+            else
+            { //display results in the label and listboxes
+                lblNumberOfMatches.Text = numOfMatchesString;
+                brighterList(exactValueMatching, recordsList);
+                dimmerList(exactValueMatching, recordsList);
+            }
+        }
+
+        private String openStarsFile(List<double> recordsList)
+        {
+            String firstLine = ""; //declare variable to be returned later
+            try
+            {
+
+                // Declare a StreamReader variable.
+                StreamReader inputFile;
+                // set up initial open directory! 
+                String UN = Environment.UserName; //variable for the username
+                String FilePath = @"C:\Users\" + UN + @"\Documents\ "; //concatinating the username with the path
+             openStarsFileDialog.InitialDirectory = FilePath; //this folder is getting showed in the second replicate of the save dialogue window
+                                                     // show only files with .txt extension
+                openStarsFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                //if the uyser click on open and open a file then
+                if (openStarsFileDialog.ShowDialog() == DialogResult.OK)
+                { //on success and choosing a file
+                    try
+                    {
+                        inputFile = File.OpenText(openStarsFileDialog.FileName);
+                        var lineCount = File.ReadAllLines(openStarsFileDialog.FileName).Length; //number of lines in the file
+                                                                                                //  MessageBox.Show("here");
+                        firstLine = inputFile.ReadLine(); //read the first line, the star name and store it in firstLine variable
+                        if (firstLine.GetType() == typeof(String))
+                        {
+                            //if firstLine is string then it could be a valid star name
+                            while (!inputFile.EndOfStream)
+                            { //add the lines to the list
+                                recordsList.Add(double.Parse(inputFile.ReadLine()));
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please chose another valid file!");
+                            firstLine = "";
+                        }
+                        inputFile.Close(); //close the file
+                        return firstLine; //return the value of the firstLine as a string
+                    }
+                    catch (Exception ex)
+                    { //throw an error message in case the user chosed a non valid file
+                        MessageBox.Show("Unable to open the selected file! Please chose another file! \n \n  Error: " + ex.Message);
+                        return firstLine;
+                    }
+                }
+                else
+                { //in case the user clicked on ancel and didn't choose any file
+                    MessageBox.Show("Operation canceled!");
+                    return firstLine;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //throw the error message in case there is any
+                MessageBox.Show(ex.Message);
+                return firstLine;
+            }
+
+        }
+
+        private String[,] fileDataKnownValues()
+        { //method that returns the default reference 2D table entered manually
+            String[,] knownValues = {
+    {
+     "OV Boo",
+     "14.95"
+    },
+    {
+     "LV 426",
+     "9.91"
+    },
+    {
+     "Fury 161",
+     "6.32"
+    },
+    {
+     "XS Dra",
+     "8.00"
+    },
+    {
+     "Hip 23429",
+     "12.19"
+    },
+    {
+     "52 Eridani",
+     "12.11"
+    },
+    {
+     "Hip 43213",
+     "11.99"
+    },
+    {
+     "Chi UMa",
+     "10.45"
+    },
+    {
+     "Xi UMa",
+     "3.27"
+    },
+    {
+     "Dubhe",
+     "2.81"
+    },
+    {
+     "Asnin 491",
+     "0.14"
+    },
+    {
+     "Beta Umi",
+     "8.85"
+    },
+    {
+     "Centaurus B",
+     "6.02"
+    },
+    {
+     "YZ Cnc",
+     "15.07"
+    },
+    {
+     "Gliese 754",
+     "18.50"
+    },
+    {
+     "Epsilon Scorpii",
+     "10.00"
+    },
+    {
+     "Markab",
+     "9.33"
+    },
+    {
+     "Phecda",
+     "11.68"
+    },
+    {
+     "Tau Ceti",
+     "19.23"
+    },
+    {
+     "Spica",
+     "2.10"
+    }
+   };
+
+            return knownValues; //return table
+
+        }
+
+        private double findTheMatchingValueWithTheExactStar(String starName, String[,] knownValues)
+        {
+            int lengthoftable = knownValues.Length - 1;
+            double knownValue = 0; //declare a variable that will hold the value of the default brightness of the star
+            for (int j = 0; j <= 19; j++)
+            { //for loop from the begining of the list to the end
+                if (starName == knownValues[j, 0])
+                { //if there is a match or the name of the star exists then the variable get the value of the brightness corresponding to that name
+                    knownValue = double.Parse(knownValues[j, 1]);
+                    break; //breaking the loop because no need to keep looking for a match, as we already found it
+                }
+            }
+            //return the brightness value
+            return knownValue;
+
+        }
+
+        private int numberOfMatches(double exactValueMatching, List<double> recordsList)
+        {
+            //method that returns the number of matches the star had
+            int numberOfMatches = 0; //incrementer setted to 0
+
+            foreach (double record in recordsList)
+            { //for every record there is a match the variable increment
+                if (record == exactValueMatching)
+                {
+                    numberOfMatches++; //incrementing
+                }
+            }
+            //return the incrementer
+            return numberOfMatches;
+        }
+
+        private void dimmerList(double exactMatch, List<double> recordsList)
+        { //method that returns the dimmer values for the analysed star compared to the default brightness
+            List<double> immerList = new List<double>();
+
+            foreach (double record in recordsList)
+            {
+                if (record > exactMatch)
+                {
+                    immerList.Add(record);
+                }
+            }
+
+            immerList.Sort(); //sorting the list asc
+            immerList.Reverse(); //reverse the soring - now its sorted desc
+
+            foreach (double record2 in immerList)
+            { //for every record in the list add them to the listbox
+                listBoxDimmerStars.Items.Add(record2.ToString());
+            }
+
+        }
+
+        private void brighterList(double exactMatch, List<double> recordsList)
+        { //method that returns the brighter values for the analysed star compared to the default brightness
+            List<double> brighterList = new List<double>();
+
+            foreach (double record in recordsList)
+            {
+                if (record < exactMatch)
+                {
+                    brighterList.Add(record);
+                }
+            }
+
+            brighterList.Sort(); //sorting the list asc
+
+
+            foreach (double record2 in brighterList)
+            { //for every record in the list add them to the listbox
+                listBoxBrighterStars.Items.Add(record2.ToString());
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear the labels and the listBoxes
+            lblNumberOfMatches.Text = "";
+            listBoxBrighterStars.Items.Clear();
+            listBoxDimmerStars.Items.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        { //close the form
+            this.Close();
+        }
+    }
+}
